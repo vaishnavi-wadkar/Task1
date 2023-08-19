a64667001da02ad474a9e48474625d8208022c21
vaishnavi-wadkar <115006842+vaishnavi-wadkar@users.noreply.github.com>
2023-08-19 20:26:38 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+package survey;
+
+import java.sql.SQLException;
+public class Main {
+	public static void main(String args[]) throws SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+}
